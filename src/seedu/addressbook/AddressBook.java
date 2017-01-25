c8e91a10c1e9c9d5fec131ee1a324cb65aaaf0a4
Tan Wei <tanwei0319@gmail.com>
2017-01-25 10:54:57 +0800

    Reduce SLAP

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -190,7 +190,21 @@ public class AddressBook {
 
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
+
+        // processProgramArgs
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
+        }
+
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
+        
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
